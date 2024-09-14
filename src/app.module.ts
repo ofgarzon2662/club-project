3c09d57b25eabdc1855d9e50c9a8df76640c7dcf
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2024-09-14 16:02:07 -0700

    Configuracion Persistencia aniadida

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocioModule } from './socio/socio.module';
-import { ClumModule } from './clum/clum.module';
 import { ClubModule } from './club/club.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SocioEntity } from './socio/socio.entity';
+import { ClubEntity } from './club/club.entity';
 
 @Module({
-  imports: [SocioModule, ClumModule, ClubModule],
+  imports: [SocioModule, ClubModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'clubes',
+      entities: [SocioEntity, ClubEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
