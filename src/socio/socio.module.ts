be88b00025b1184744593516d15da1fb00b03d66
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2024-09-14 17:42:35 -0700

    Clase socio Aniadida

--- src/socio/socio.module.ts
+++ src/socio/socio.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SocioService } from './socio.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SocioEntity } from './socio.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([SocioEntity])],
+  providers: [SocioService]
+})
+
 export class SocioModule {}
