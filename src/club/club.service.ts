aa6f169e58d24a7160e72d72b9d5abaa5885f852
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2024-09-14 17:34:07 -0700

    logic added for club

--- src/club/club.service.ts
+++ src/club/club.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { In, Repository } from 'typeorm';
 import { ClubEntity } from './club.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BusinessError, BusinessLogicException } from 'src/shared/errors/business-errors';
 
 @Injectable()
 export class ClubService {
@@ -20,8 +21,41 @@ export class ClubService {
     async findOne(id: string): Promise<ClubEntity> {
         const club: ClubEntity = await this.clubRepository.findOne({where: {id}, relations: ["socios"] } );
         if (!club)
-          throw new BusinessLogicException("The museum with the given id was not found", BusinessError.NOT_FOUND);
+          throw new BusinessLogicException("El club con el id provisto no existe", BusinessError.NOT_FOUND);
     
         return club;
+    }
+
+    // crear un club
+
+    async create(club: ClubEntity): Promise<ClubEntity> {
+        if (club.descripcion.length > 100) {
+            throw new BusinessLogicException("La descripción no puede tener más de 100 caracteres", BusinessError.BAD_REQUEST);
+            }
+        return await this.clubRepository.save(club);
+    }
+
+    // Actualizar un club
+
+    async update(id: string, club: ClubEntity): Promise<ClubEntity> {
+        const clubToUpdate: ClubEntity = await this.clubRepository.findOne({where: {id}});
+        if (!clubToUpdate)
+          throw new BusinessLogicException("El club con el id provisto no existe", BusinessError.NOT_FOUND);
+        
+        if (club.descripcion.length > 100) {
+            throw new BusinessLogicException("La descripción no puede tener más de 100 caracteres", BusinessError.BAD_REQUEST);
+        }
+    
+        return await this.clubRepository.save(club);
+    }
+
+    // Eliminar un club
+
+    async delete(id: string) {
+        const club: ClubEntity = await this.clubRepository.findOne({where:{id}});
+        if (!club)
+          throw new BusinessLogicException("El club con el id provisto no existe", BusinessError.NOT_FOUND);
+      
+        await this.clubRepository.remove(club);
     }
 }
