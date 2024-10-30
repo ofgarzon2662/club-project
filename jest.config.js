5f76193b21ead6076a3f307ce840e8140d996ff2
Fernando Garzon <of.garzon2662@uniandes.edu.co>
2024-10-29 20:57:29 -0700

    last try

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,19 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'cobertura'],
-  reporters: ['default'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  reporters: [
+    'default',
+    [
+      'jest-sonar',
+      {
+        outputDirectory: 'coverage', // Directory where the report will be saved
+        outputName: 'test-report.xml', // Name of the report file
+        reportedFilePath: 'absolute', // Use absolute file paths in the report
+      },
+    ],
+  ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  testResultsProcessor: 'jest-sonar-reporter',
 };
